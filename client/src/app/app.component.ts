159840e6379845d88cb6f588bc39e501523fbbee
Rana Sri <ranabis@outlook.com>
2024-10-19 01:09:50 -0400

    UpdatingUserIdentity

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  http =inject(HttpClient);
+  http = inject(HttpClient);
   title = 'DatingApp';
   users: any;
 
