c58439fce59e9e96cade342d8eb27fb3131b7e5b
Rana Sri <ranabis@outlook.com>
2025-03-02 00:23:48 -0500

    Added Nav with Login/Logout

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
@@ -16,11 +17,15 @@ export class NavComponent {
   
   login() {
     this.accountService.login(this.model).subscribe({
-      next: resonse => {
-        console.log(resonse);
+      next: response => {
+        console.log(response);
         this.loggedIn = true;
       },
       error: error => console.log(error)
     })
+  }
+
+  logout() {
+    this.loggedIn = false;
   }
 }
