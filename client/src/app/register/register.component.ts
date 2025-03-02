6ec8f513fd808a598b47a1b62535bc50fa5809c2
Rana Sri <ranabis@outlook.com>
2025-03-02 18:23:21 -0500

    Completed Register Form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +10,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  private accountService = inject(AccountService);
+  // usersFromHomeComponent = input.required<any>();
+  cancelRegister = output<boolean>();
   model: any = {}
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
   }
 
   cancel() {
-    console.log('Cancelled.');
+    this.cancelRegister.emit(false);
   }
 }
