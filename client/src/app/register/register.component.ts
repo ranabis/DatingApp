2144d2a9bc7cec8d54d4b515ed97aa624a9d78dc
Rana Sri <ranabis@outlook.com>
2025-03-06 20:48:02 -0500

    Minor Updates

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
