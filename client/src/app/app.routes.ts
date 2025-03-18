26dbefad9051eb71d11bd9481e86c44d8388d072
Rana Sri <ranabis@outlook.com>
2025-03-17 20:24:03 -0400

    Extended the User Entity

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
-import { TestErrorsComponent } from './errors/test-errors.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -49,6 +49,5 @@ export class TestErrorsComponent {
         this.validationErrors = error;
       }
     })
-  }
-  
+  }  
 }
